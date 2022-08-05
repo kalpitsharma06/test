0399caf8d990e3ac0e88bf83a0df556d60f086ad
madhav <madhav.appic@gmail.com>
2022-08-05 14:23:55 +0530

    swagger doc cart api

--- apis/model/additionalinfo.js
+++ apis/model/additionalinfo.js
@@ -8,21 +8,21 @@ var additional_info = new mongoose.Schema({
 timmings:[{
         
         monday_open: {
-            type: String 
+            type: String ,default:"closed",
         },
         monday_close:{
-            type :String
+            type: String ,default:"closed",
         },
-        tuesday_open: { type:String},
-        tuesday_open: {type:String},
-        thursday_open: {type:String},
-        thursday_close: {type:String},
-        friday_open:{type:String},
-        friday_close:{type:String},
-        saturday_open:{type:String},
-        saturday_close:{type:String},
-        sunday_open:{type:String},
-        sunday_close:{type:String},
+        tuesday_open: {    type: String ,default:"closed",},
+        tuesday_open: {   type: String ,default:"closed",},
+        thursday_open: {   type: String ,default:"closed",},
+        thursday_close: {   type: String ,default:"closed",},
+        friday_open:{   type: String ,default:"closed",},
+        friday_close:{ type: String ,default:"closed",},
+        saturday_open:{ type: String ,default:"closed",},
+        saturday_close:{ type: String ,default:"closed",},
+        sunday_open:{ type: String ,default:"closed",},
+        sunday_close:{ type: String ,default:"closed",},
     }
 ],
 
